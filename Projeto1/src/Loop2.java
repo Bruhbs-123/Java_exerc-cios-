ce6f5da5909f7ce323d9df4a824a5b039de99de7
Bruna Bonifácio <bruna.bonifacio@sempreceub.com>
2026-03-15 15:34:24 -0300

    Add files via upload

---
+++ Projeto1/src/Loop2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Loop2 {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Digite a quantidade de degraus: " ) ;
+        int degraus = scanner.nextInt();
+
+            for (int i = 1; i <= degraus; i++) {
+
+            System.out.println("Subindo o degrau " + i);
+        }
+
+System.out.println("Você chegou ao topo! " ) ;
+
+
+    }
+
+}
