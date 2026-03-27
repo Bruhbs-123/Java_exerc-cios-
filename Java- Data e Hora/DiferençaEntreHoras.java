b26220b8d9c234b6edb43aa518ad1ea2de0ad12a
Bruna Bonifácio <bruna.bonifacio@sempreceub.com>
2026-03-27 17:17:59 -0300

    Update DiferençaEntreHoras.java

--- Java- Data e Hora/DiferençaEntreHoras.java
+++ Java- Data e Hora/DiferençaEntreHoras.java
@@ -1,3 +1,13 @@
+/*Você é responsável pelo controle de tempo em projetos dentro de uma agência de desenvolvimento. Você precisa monitorar o tempo gasto em cada tarefa para garantir que os projetos sejam executados dentro do prazo.
+
+Para isso, crie um programa que:
+
+    Receba dois horários representando o início e o término de uma atividade.
+    Calcule a diferença em horas e minutos entre esses dois horários.    Exiba o resultado formatado.
+
+*/
+
+
 import java.time.LocalTime;
 import java.time.Duration;
 
