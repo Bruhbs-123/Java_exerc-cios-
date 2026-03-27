6085e8d4e279f5e7e59d338401768d83ca5e002a
Bruna Bonifácio <bruna.bonifacio@sempreceub.com>
2026-03-27 17:19:10 -0300

    Update DataEntrega.java

--- Java- Data e Hora/DataEntrega.java
+++ Java- Data e Hora/DataEntrega.java
@@ -1,3 +1,12 @@
+/*
+Você é responsável pelos projetos em uma empresa de desenvolvimento de software. Para garantir que os prazos sejam cumpridos, você precisa calcular a data de entrega de cada projeto com base na data de início e no prazo estimado em dias.
+
+Você precisa criar um programa que:
+
+    Receba uma data de início.
+    Adicione o prazo em dias ao início do projeto.
+    Exiba a data final formatada corretamente.
+ */
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
