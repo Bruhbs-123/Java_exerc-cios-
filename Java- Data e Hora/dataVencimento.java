f98f5a27208b56ccea4c79880b57937a79633e72
Bruna Bonifácio <bruna.bonifacio@sempreceub.com>
2026-03-27 17:23:29 -0300

    Update dataVencimento.java

--- Java- Data e Hora/dataVencimento.java
+++ Java- Data e Hora/dataVencimento.java
@@ -1,3 +1,12 @@
+/*   
+Você trabalha no setor financeiro de uma empresa de serviços e é responsável por gerenciar o vencimento das faturas dos clientes. Em alguns casos, os clientes solicitam um adiamento da data de pagamento, e o sistema precisa calcular a nova data de vencimento com base na quantidade de meses adicionados.
+
+Crie um programa que:
+
+    Receba uma data de vencimento original.
+    Adicione um número de meses ao vencimento.
+    A data ajustada deve ser exibida no formato dd-MM-yyyy.
+*/
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
