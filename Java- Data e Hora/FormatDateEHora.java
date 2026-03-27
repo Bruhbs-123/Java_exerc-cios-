7b48f30afc4c5e612f774146a2fe54a995f52aa7
Bruna Bonifácio <bruna.bonifacio@sempreceub.com>
2026-03-27 17:21:11 -0300

    Refactor date and time formatting in FormatDateEHora
    Updated the date and time formatting to match Brazilian standards.

--- Java- Data e Hora/FormatDateEHora.java
+++ Java- Data e Hora/FormatDateEHora.java
@@ -1,3 +1,13 @@
+/*
+Você trabalha no setor de tecnologia de uma empresa que gera relatórios diários. Esses relatórios incluem informações como transações realizadas, atualizações de pedidos e registros de atendimento ao cliente. A equipe responsável pela análise dos relatórios solicitou que a data e a hora sejam exibidas em um formato mais familiar para os usuários brasileiros.
+
+Para resolver essa tarefa, você deve:
+
+    Capturar a data e a hora atuais.
+    Formatar a data no padrão dd-MM-yyyy (dia-mês-ano).
+    Formatar a hora no padrão HH:mm (hora e minutos).
+    Exibir o resultado da formatação.
+ */
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
